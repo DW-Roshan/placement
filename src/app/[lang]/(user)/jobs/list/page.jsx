9bb0273bb45de311de805b1bd3bd1a3c5a88f30c
DW-Roshan <roshan@dreamweaversindia.com>
2025-09-29 11:30:42 +0530

    Testing Skeleton in Jobs Listing on Executive

--- src/app/[lang]/(user)/jobs/list/page.jsx
+++ src/app/[lang]/(user)/jobs/list/page.jsx
@@ -4,51 +4,54 @@ import { authOptions } from "@/libs/auth";
 
 import JobsList from "@/views/user/jobs/list"
 
-const getJobsData = async () => {
-  // Vars
-  try {
-    // const token = await getCookie('token');
+// const getJobsData = async () => {
+//   // Vars
+//   try {
+//     // const token = await getCookie('token');
 
-    const session = await getServerSession(authOptions);
-    const token = session?.user?.token;
+//     const session = await getServerSession(authOptions);
+//     const token = session?.user?.token;
 
-    if(token){
+//     if(token){
 
-      const res = await fetch(`${process.env.API_URL}/jobs`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
-        }
-      })
+//       const res = await fetch(`${process.env.API_URL}/jobs`, {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'Authorization': `Bearer ${token}`
+//         }
+//       })
 
-      if(res.ok){
+//       if(res.ok){
 
-        const data = await res.json();
+//         const data = await res.json();
 
-        return data;
-      } else {
+//         return data;
+//       } else {
 
-        return [];
-      }
+//         return [];
+//       }
 
-    } else{
-      return [];
-    }
+//     } else{
+//       return [];
+//     }
 
-  } catch (error) {
-    console.error('Error fetching user data:', error);
+//   } catch (error) {
+//     console.error('Error fetching user data:', error);
 
-    return [];
-  }
-}
+//     return [];
+//   }
+// }
 
 const ListPage = async () => {
 
-  const jobsData = await getJobsData()
+  // const jobsData = await getJobsData()
+
+  const session = await getServerSession(authOptions);
+  const token = session?.user?.token;
 
   return (
-    <JobsList jobsData={jobsData.data} />
+    <JobsList token={token} />
   )
 }
 
--- src/views/user/jobs/list/index.jsx
+++ src/views/user/jobs/list/index.jsx
@@ -1,11 +1,66 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+
 // MUI Imports
 import Grid from '@mui/material/Grid2'
 
 // Component Imports
 // import CandidatesListTable from './CandidatesListTable'
 import JobsListCard from './JobsListCard'
+import CardSkeletons from '@/components/skeletons/CardSkeletons';
 
-const JobsList = ({jobsData, isCandidate, hideSearch}) => {
+
+const JobsList = ({token, isCandidate, hideSearch}) => {
+
+  const [jobsData, setJobsData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  
+  const getJobsData = async () => {
+    try {
+      if (!token) {
+        setJobsData([])
+
+        return
+      }
+
+      setLoading(true) // start loading
+
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/jobs`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      })
+
+      if (res.ok) {
+        const data = await res.json()
+
+        setJobsData(data.data)
+      } else {
+        setJobsData([])
+      }
+    } catch (error) {
+      console.error('Error fetching jobs:', error)
+      setJobsData([])
+    } finally {
+      setLoading(false) // always stop loading
+    }
+  }
+
+  useEffect(() => {
+    getJobsData();
+  }, [token])
+
+  // const jobsData = await getJobsData()
+
+  if(loading) {
+    return <CardSkeletons totalCards={6} />
+  }
+  
+
   return (
     <Grid container spacing={6}>
       <Grid size={{ xs: 12 }}>
